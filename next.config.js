ddd6e44cd9b733094d5cd999e36243e06db2f609
dennise909 <dennise909@hotmail.com>
2024-08-12 21:15:44 +0200

    pnpm file deleted

--- next.config.js
+++ next.config.js
@@ -13,5 +13,6 @@ module.exports = {
   env: {
     GOOGLE_CLIENT_ID: '911452289802-r37kaflvcvkvm9mrhehataqmka7hlnti.apps.googleusercontent.com',
     GOOGLE_API_KEY: 'AIzaSyChQ1v_FDqWGz-TA7yFPcSoPBytfqQ8J-4',
+    VERCEL_FORCE_NO_BUILD_CACHE: '1'
   },
 }
